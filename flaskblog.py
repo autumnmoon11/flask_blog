e8a60aeadec601e820c91ddb0bfd713fb079f4f4
Autumn Conner <autumnmoon1192@gmail.com>
2026-01-15 10:29:31 -0700

    Added registration page using WTForms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+import os
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 posts = [
     {
@@ -26,6 +30,18 @@ def home():
 def about():
     return render_template("about.html", title="About")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
