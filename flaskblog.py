653ead73a2fcb45e36908b30d6c424b0d3ab8ff5
Autumn Conner <autumnmoon1192@gmail.com>
2026-01-17 03:35:24 -0700

    Added login page and updated nav bar

--- flaskblog.py
+++ flaskblog.py
@@ -41,6 +41,12 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
