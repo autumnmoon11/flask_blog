3d11cd8dfc0bdd6ad0f02ede2fe8222e7fb1613a
Autumn Conner <autumnmoon1192@gmail.com>
2026-02-02 23:07:05 -0700

    Refactor: Move to application factory and externalize config

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,58 +1,9 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_bcrypt import Bcrypt
-# from flask_login import LoginManager
-# from flask_mail import Mail
-# import os
-
-# app = Flask(__name__)
-
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
-# db = SQLAlchemy(app)
-# bcrypt = Bcrypt(app)
-# login_manager = LoginManager(app)
-# login_manager.login_view = 'users.login'
-# login_manager.login_message_category = 'info'
-# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_PORT'] = 587
-# app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-# app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-
-# mail = Mail(app)
-
-
-# from flaskblog import routes, models
-
-# from flaskblog.users.routes import users
-# from flaskblog.posts.routes import posts
-# from flaskblog.main.routes import main
-
-# app.register_blueprint(users)
-# app.register_blueprint(posts)
-# app.register_blueprint(main)
-
-# @app.shell_context_processor
-# def make_shell_context():
-#     return {
-#         'db': db, 
-#         'User': models.User, 
-#         'Post': models.Post,
-#         'mail': mail
-#     }
-
-
-
-
-
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+from flaskblog.config import Config
 
 # 1. Initialize extensions without the 'app' yet
 db = SQLAlchemy()
@@ -62,17 +13,11 @@ login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
     
     # 2. Load configurations
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
-    app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-    app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+    app.config.from_object(config_class)
 
     # 3. Bind extensions to the app
     db.init_app(app)
