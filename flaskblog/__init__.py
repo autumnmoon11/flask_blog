fdda7b968a3bfe95ee786eaeb8e3917342904da3
Autumn Conner <autumnmoon1192@gmail.com>
2026-02-10 02:35:22 -0700

    feat: initialize flask-migrate for database version control

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 from flaskblog.config import Config
 
 # 1. Initialize extensions without the 'app' yet
@@ -12,6 +13,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -24,6 +26,7 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app, db)
 
     # 4. Import and Register Blueprints inside the function
     from flaskblog.users.routes import users
